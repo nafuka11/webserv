43e35681d17ec5daafc9be4ff8d840f935a7391d
nfukada <nfukada@student.42tokyo.jp>
2021-12-02 22:45:43 +0900

    fix(HTTPRequest): HTTPParseで行う処理を削除

--- includes/HTTPRequest.hpp
+++ includes/HTTPRequest.hpp
@@ -3,53 +3,33 @@
 
 #include <string>
 #include <map>
-#include <vector>
-#include <iostream>
 #include "HTTPMethod.hpp"
 
 class HTTPRequest
 {
 public:
-    enum ParseState
-    {
-        PARSE_START_LINE,
-        PARSE_HEADERS,
-        PARSE_MESSAGE_BODY,
-        PARSE_FINISH
-    };
-
     HTTPRequest();
     ~HTTPRequest();
     void clear();
-    void parse();
-    void appendRawMessage(const char *message);
+
+    void setMethod(HTTPMethod method);
+    void setUri(const std::string &uri);
+    void setProtocolVersion(const std::string &protocol_version);
+    void setHeader(const std::string &name, const std::string &value);
+    void setMessageBody(const std::string &message_body);
+
+    HTTPMethod getMethod() const;
+    const std::string &getUri() const;
+    const std::string &getProtocolVersion() const;
+    const std::map<std::string, std::string> getHeaders() const;
+    const std::string &getMessageBody() const;
 
 private:
-    std::string raw_message_;
-    size_t parse_pos_;
-    ParseState state_;
-
     HTTPMethod method_;
     std::string uri_;
     std::string protocol_version_;
     std::map<std::string, std::string> headers_;
     std::string message_body_;
-
-    void parseLine(const std::string &line);
-    void parseStartLine(const std::string &line);
-    void parseHeader(const std::string &line);
-    void parseMessageBody(const std::string &line);
-    bool needsParsingMessageBody();
-    bool isSpace(char c);
-    bool isToken(const std::string &str);
-    bool isTokenChar(char c);
-    std::vector<std::string> split(const std::string &str, const std::string &delim);
-
-    void setMethod(const std::string &method);
-    void setUri(const std::string &uri);
-    void setProtocolVersion(const std::string &protocol_version);
-    void setHeader(const std::string &key, const std::string &value);
-    void setMessageBody(const std::string &message_body);
 };
 
 #endif /* HTTPREQUEST_HPP */
--- srcs/HTTPRequest.cpp
+++ srcs/HTTPRequest.cpp
@@ -1,7 +1,6 @@
 #include "HTTPRequest.hpp"
-#include "HTTPParseException.hpp"
 
-HTTPRequest::HTTPRequest() : parse_pos_(0), state_(PARSE_START_LINE)
+HTTPRequest::HTTPRequest()
 {
 }
 
@@ -11,189 +10,17 @@ HTTPRequest::~HTTPRequest()
 
 void HTTPRequest::clear()
 {
-    raw_message_.clear();
-    parse_pos_ = 0;
-    state_ = PARSE_START_LINE;
+    uri_.clear();
+    protocol_version_.clear();
+    headers_.clear();
     message_body_.clear();
 }
 
-void HTTPRequest::appendRawMessage(const char *message)
+void HTTPRequest::setMethod(HTTPMethod method)
 {
-    raw_message_.append(message);
+    method_ = method;
 }
 
-void HTTPRequest::parse()
-{
-    size_t newline_pos;
-
-    while (true)
-    {
-        newline_pos = raw_message_.find("\r\n", parse_pos_);
-        if (newline_pos == std::string::npos)
-        {
-            break;
-        }
-        std::string line = raw_message_.substr(parse_pos_, newline_pos - parse_pos_);
-        parseLine(line);
-        if (state_ == PARSE_FINISH)
-        {
-            std::cout << "method: \"" << method_ << "\"" << std::endl
-                    << "uri: \"" << uri_ << "\"" << std::endl
-                    << "protocol: \"" << protocol_version_ << "\"" << std::endl;
-            for (std::map<std::string, std::string>::iterator iter = headers_.begin();
-                iter != headers_.end();
-                ++iter)
-            {
-                std::cout << (*iter).first << ": \"" << (*iter).second << "\"" << std::endl;
-            }
-            clear();
-            break;
-        }
-        parse_pos_ = newline_pos + 2;
-    }
-}
-
-void HTTPRequest::parseLine(const std::string &line)
-{
-    switch (state_)
-    {
-    case PARSE_START_LINE:
-        parseStartLine(line);
-        break;
-    case PARSE_HEADERS:
-        parseHeader(line);
-        break;
-    case PARSE_MESSAGE_BODY:
-        parseMessageBody(line);
-        break;
-    default:
-        break;
-    }
-}
-
-void HTTPRequest::parseStartLine(const std::string &line)
-{
-    if (line.empty())
-    {
-        return;
-    }
-
-    std::vector<std::string> items = split(line, " ");
-    if (items.size() != 3)
-    {
-        throw HTTPParseException(CODE_400);
-    }
-    setMethod(items[0]);
-    setUri(items[1]);
-    setProtocolVersion(items[2]);
-    state_ = PARSE_HEADERS;
-}
-
-void HTTPRequest::parseHeader(const std::string &line)
-{
-    if (line.empty())
-    {
-        if (needsParsingMessageBody())
-        {
-            state_ = PARSE_MESSAGE_BODY;
-        }
-        else
-        {
-            state_ = PARSE_FINISH;
-        }
-        return;
-    }
-    size_t delim_pos = line.find(":", 0);
-    if (delim_pos == std::string::npos)
-    {
-        throw HTTPParseException(CODE_400);
-    }
-    std::string key = line.substr(0, delim_pos);
-    size_t value_start = delim_pos + 1;
-    while (isSpace(line[value_start]))
-    {
-        value_start++;
-    }
-    size_t value_end = line.size();
-    while (value_end - 1 > 0 && isSpace(line[value_end - 1]))
-    {
-        value_end--;
-    }
-    std::string value = line.substr(value_start, value_end - value_start);
-    setHeader(key, value);
-}
-
-void HTTPRequest::parseMessageBody(const std::string &line)
-{
-    (void)line;
-    state_ = PARSE_FINISH;
-}
-
-bool HTTPRequest::needsParsingMessageBody()
-{
-    return method_ == POST &&
-        (headers_.count("content-length") || headers_.count("transfer-encoding"));
-}
-
-bool HTTPRequest::isSpace(char c)
-{
-    return c == ' ' || c == '\t';
-}
-
-bool HTTPRequest::isToken(const std::string &str)
-{
-    for (size_t i = 0; i < str.size(); i++)
-    {
-        if (!isTokenChar(str[i]))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool HTTPRequest::isTokenChar(char c)
-{
-    return isalnum(c) || strchr("!#$%&'*+-.^_`|~", c);
-}
-
-std::vector<std::string> HTTPRequest::split(const std::string &str, const std::string &delim)
-{
-    std::vector<std::string> words;
-    size_t start = 0;
-    size_t end = 0;
-
-    while ((end = str.find(delim, start)) != std::string::npos)
-    {
-        words.push_back(str.substr(start, end - start));
-        start = end + delim.size();
-    }
-    if (start <= str.size())
-    {
-        words.push_back(str.substr(start, str.size() - start));
-    }
-    return words;
-}
-
-void HTTPRequest::setMethod(const std::string &method)
-{
-    if (method == "GET")
-    {
-        method_ = GET;
-    }
-    else if (method == "POST")
-    {
-        method_ = POST;
-    }
-    else if (method == "DELETE")
-    {
-        method_ = DELETE;
-    }
-    else
-    {
-        throw HTTPParseException(CODE_501);
-    }
-}
 void HTTPRequest::setUri(const std::string &uri)
 {
     uri_ = uri;
@@ -201,23 +28,40 @@ void HTTPRequest::setUri(const std::string &uri)
 
 void HTTPRequest::setProtocolVersion(const std::string &protocol_version)
 {
-    if (protocol_version != "HTTP/1.1")
-    {
-        throw HTTPParseException(CODE_400);
-    }
     protocol_version_ = protocol_version;
 }
 
-void HTTPRequest::setHeader(const std::string &key, const std::string &value)
+void HTTPRequest::setHeader(const std::string &name, const std::string &value)
 {
-    if (!isToken(key))
-    {
-        throw HTTPParseException(CODE_400);
-    }
-    headers_.insert(std::make_pair(key, value));
+    headers_.insert(std::make_pair(name, value));
 }
 
 void HTTPRequest::setMessageBody(const std::string &message_body)
 {
-    (void)message_body;
+    message_body_ = message_body;
+}
+
+HTTPMethod HTTPRequest::getMethod() const
+{
+    return method_;
+}
+
+const std::string &HTTPRequest::getUri() const
+{
+    return uri_;
+}
+
+const std::string &HTTPRequest::getProtocolVersion() const
+{
+    return protocol_version_;
+}
+
+const std::map<std::string, std::string> HTTPRequest::getHeaders() const
+{
+    return headers_;
+}
+
+const std::string &HTTPRequest::getMessageBody() const
+{
+    return message_body_;
 }
