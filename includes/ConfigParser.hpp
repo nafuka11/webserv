ba965b328fdd88e65f5d7fe3108ac195166c55d1
ysaito <shiva.lts239@gmail.com>
2021-12-11 00:01:24 +0900

    feat(ConfigParser::): serverコンテキストのlistenディレクティブの読み込み

--- includes/ConfigParser.hpp
+++ includes/ConfigParser.hpp
@@ -10,34 +10,20 @@ class Config;
 class ConfigParser
 {
 public:
-    enum ParseState
+    enum ContextState
     {
-        PARSE_MAIN_BLOCK,
-        PARSE_SERVER_BLOCK,
-        // PARSE_SERVER_BLOCK_START,
-        // PARSE_SERVER_BLOCK_DOONE,
-        PARSE_LOCATION_BLOCK,
-        // PARSE_LOCATION_BLOCK_START,
-        // PARSE_LOCATION_BLOCK_DONE
-        PARSE_DONE//del
+        MAIN_BLOCK,
+        SERVER_BLOCK,
+        LOCATION_BLOCK
     };
 
     ConfigParser(Config &config);
     ~ConfigParser();
     void readFile(const std::string &filepath);
+    void readAndSplit(std::ifstream &ifs, std::vector<std::vector<std::string> > &config_file);
     std::vector<std::string> splitLine(const std::string &line);
-    void parseWords(const std::vector<std::string> &words);
-    void parseMainBlock(const std::vector<std::string> &words);
-    void parseServerBlock(const std::vector<std::string> &words);
-    // void parseLocationBlock(const std::vector<std::string> &words);
-    void parseListenDirective(ServerConfig &server, const std::vector<std::string> &words);
-    void parseServerNameDirective(ServerConfig &server, const std::vector<std::string> &words);
+    void putSplitLine(std::vector<std::vector<std::string> > &config_file);//必要なくなったら消す
 private:
-    class OpenConfigFileException : public std::exception
-    {
-        public:
-            virtual const char *what() const throw();
-    };
     class ErrorBlockStartException : public std::exception
     {
         public:
@@ -47,7 +33,7 @@ private:
     static const std::string SERVER_BLOCK_DIRECTIVE[3];
 
     Config &config_;
-    ParseState state_;
+    // ParseState state_;
 };
 
 #endif /* CONFIGPARSER_HPP */
--- srcs/ConfigParser.cpp
+++ srcs/ConfigParser.cpp
@@ -3,13 +3,14 @@
 #include <iostream>//del;
 #include <string>
 #include "Config.hpp"
+#include "SystemError.hpp"
 
 const std::string SERVER_BLOCK_DIRECTIVE[3] = {
     "listen",
     "server_name"
 };
 
-ConfigParser::ConfigParser(Config &config) : config_(config), state_(PARSE_MAIN_BLOCK)
+ConfigParser::ConfigParser(Config &config) : config_(config)//, state_(PARSE_MAIN_BLOCK)
 {
 }
 
@@ -17,30 +18,87 @@ ConfigParser::~ConfigParser()
 {
 }
 
+// 必要なくなったら消す
+void ConfigParser::putSplitLine(std::vector<std::vector<std::string> > &config_file)
+{
+    for (size_t line_num = 0; line_num < config_file.size(); ++line_num)
+    {
+        std::cout << (line_num + 1) << ")------------------" << std::endl;
+        for (size_t word = 0; word < config_file.at(line_num).size(); ++word)
+        {
+            std::cout << word << ": [" << config_file.at(line_num).at(word) << "]" << std::endl;
+        }
+    }
+}
+// 必要なくなったら消す
+
 void ConfigParser::readFile(const std::string &filepath)
 {
     std::ifstream ifs(filepath);
-
     if (!ifs)
     {
-        throw OpenConfigFileException();
+        throw SystemError("open", errno);
     }
+
     std::vector<std::vector<std::string> > config_file;
+    readAndSplit(ifs, config_file);
+    putSplitLine(config_file);//del
+    ifs.close();
+
+    for (size_t line_num = 0; line_num < config_file.size(); ++line_num)
+    {
+        if (config_file.at(line_num).size() == 0)
+        {
+            continue ;
+        }
+        if ((config_file.at(line_num).at(0) == "server")
+            && (config_file.at(line_num).at(1) == "{")
+            && (config_file.at(line_num).size() == 2))
+        {
+                std::cout << "Line." << (line_num + 1) << ": FIND SERVER" << std::endl;//del
+
+                // state_ = SERVER_BLOCK;
+                ++line_num;
+                ServerConfig server = ServerConfig();
+                for (; line_num < config_file.size(); ++line_num)
+                {
+                    for (size_t word = 0; word < config_file.at(line_num).size(); ++word)
+                    {
+                        if ((config_file.at(line_num).at(word) == "}")
+                             && (config_file.at(line_num).size() == 1))
+                        {
+                            //state_SERVER_END;
+                            config_.addServer(server);
+                            break ;
+                        }
+                        else if (config_file.at(line_num).at(word) == "listen")
+                        {
+                            //構文チェック(引数は正しいか、最後に";"があるか)
+                            ++word;
+                            server.setListen(atoi(config_file.at(line_num).at(word).c_str()));
+                        }
+                        else if (config_file.at(line_num).at(word) == "server_name")
+                        {
+                            //構文チェック(引数は正しいか、最後に";"があるか)
+                            ++word;
+                            server.setServerName(config_file.at(line_num).at(word));
+                        }
+                    }
+                }
+        }
+    }
+}
+void ConfigParser::readAndSplit(std::ifstream &ifs, std::vector<std::vector<std::string> > &config_file)
+{
     std::string line;
+
     for (int line_num = 0; std::getline(ifs, line); ++line_num)
     {
         const std::vector<std::string> words = splitLine(line);
         config_file.push_back(words);
     }
-    //de;
-    for (size_t i = 0; i < config_file.size(); ++i)
-    {
-        for (size_t j = 0; j < config_file.at(i).size(); j++)
-            std::cout << j << ": [" << config_file.at(i).at(j) << "]" << std::endl;
-    }
-    //de;
-    ifs.close();
 }
+
 
 std::vector<std::string> ConfigParser::splitLine(const std::string &line)
 {
@@ -68,102 +126,6 @@ std::vector<std::string> ConfigParser::splitLine(const std::string &line)
         }
     }
     return (words);
-}
-
-void ConfigParser::parseWords(const std::vector<std::string> &words)
-{
-    switch (state_)
-    {
-    case PARSE_MAIN_BLOCK:
-        std::cout << "PARSE_MAIN_BLOCK: " << words[0] << std::endl;
-        parseMainBlock(words);
-        break;
-    case PARSE_SERVER_BLOCK:
-        std::cout << "PARSE_SERVER_BLOCK: " << words[0] << std::endl;
-        parseServerBlock(words);
-        break;
-    case PARSE_LOCATION_BLOCK:
-    //     std::cout << "PARSE_LOCATION_BLOCK: " << words[0] << std::endl;
-    //     parseLocationBlock(words);
-        state_ = PARSE_DONE;//del;
-        break;
-    default:
-        break;
-    }
-}
-
-void ConfigParser::parseMainBlock(const std::vector<std::string> &words)
-{
-    if ((words[0] == "server") &&
-        ((words[1] == "{") && (words.size() == 2)))
-    {
-        state_ = PARSE_SERVER_BLOCK;
-        return ;
-    }
-    throw ErrorBlockStartException();
-    // else if... 配列でmainブロック内に記載可能なディレクティブを持って、処理関数を紐付け。でもいいかも
-}
-
-void ConfigParser::parseServerBlock(const std::vector<std::string> &words)
-{
-    ServerConfig server = ServerConfig();
-
-    // void (*parse_directive_func[3])(ServerConfig&, const std::vector<std::string>&) = {
-	//     &ConfigParser::parseListenDirective,
-	//     &ConfigParser::parseServerNameDirective,
-    // };
-
-    // for (int i = 0; i < 3; ++i)
-    // {
-    //     if (words[0] == SERVER_BLOCK_DIRECTIVE[i])
-    //     {
-    //         parse_directive_func[i](server, words);
-    //     }
-    // }
-    if (words[0] == "listen")
-    {
-        parseListenDirective(server, words);
-    }
-    else if (words[0] == "server_name")
-    {
-        parseServerNameDirective(server, words);
-    }
-    else if (words[0] == "location")
-    {
-        if (words[1] == "{" && words.size() == 2)
-        {
-            state_ = PARSE_LOCATION_BLOCK;
-            return ;
-        }
-        else
-        {
-            throw ErrorBlockStartException();
-        }
-    }
-
-}
-
-// void ConfigParser::parseLocationBlock(const std::vector<std::string> &words)
-// {
-
-//     return ;
-// }
-
-void ConfigParser::parseListenDirective(ServerConfig &server, const std::vector<std::string> &words)
-{
-    server.setListen(std::atoi(words[1].c_str())); // ConfigAtoi作るか?
-    config_.addServer(server);
-}
-void ConfigParser::parseServerNameDirective(ServerConfig &server, const std::vector<std::string> &words)
-{
-    server.setServerName(words[1]);
-    config_.addServer(server);
-}
-
-const char *ConfigParser::OpenConfigFileException::what() const throw()
-{
-    //文言考える
-    return ("ConfigParser::OpenConfigFileException");
 }
 
 const char *ConfigParser::ErrorBlockStartException::what() const throw()
