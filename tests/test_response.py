9a022e70e1b19a7ab7a5becefb8f2080860a2842
nfukada <nfukada@student.42tokyo.jp>
2021-12-15 18:48:57 +0900

    test: headerのテストを追加

--- tests/test_response.py
+++ tests/test_response.py
@@ -93,14 +93,73 @@ def test_invalid_http_version(http_connection: HTTPConnection):
     response = http_connection.getresponse()
     actual_body = response.read()
     expected_body = RESPONSE_BODY_400
-    expected_body = (
-        b"<html>\r\n"
-        b"<head><title>400 Bad Request</title></head>\r\n"
-        b"<body>\r\n"
-        b"<center><h1>400 Bad Request</h1></center><hr><center>webserv/1.0.0</center>\r\n"
-        b"</body>\r\n"
-        b"</html>\r\n"
-    )
+    assert response.status == 400
+    assert response.reason == "Bad Request"
+    assert response.version == 11
+    assert response.getheader("Content-Length") == str(len(expected_body))
+    assert actual_body == RESPONSE_BODY_400
+
+
+def test_empty_header_value(http_connection: HTTPConnection):
+    headers = {"Empty": ""}
+    http_connection.request("GET", "/", headers=headers)
+    response = http_connection.getresponse()
+    actual_body = response.read()
+    expected_body = RESPONSE_BODY_400
+    assert response.status == 400
+    assert response.reason == "Bad Request"
+    assert response.version == 11
+    assert response.getheader("Content-Length") == str(len(expected_body))
+    assert actual_body == expected_body
+
+
+def test_duplicate_host_header(http_connection: HTTPConnection):
+    http_connection.putrequest("GET", "/", skip_host=True)
+    http_connection.putheader("host", "example.com")
+    http_connection.putheader("host", "example.com")
+    http_connection.endheaders()
+    response = http_connection.getresponse()
+    actual_body = response.read()
+    expected_body = RESPONSE_BODY_400
+    assert response.status == 400
+    assert response.reason == "Bad Request"
+    assert response.version == 11
+    assert response.getheader("Content-Length") == str(len(expected_body))
+    assert actual_body == expected_body
+
+
+def test_missing_host_header(http_connection: HTTPConnection):
+    http_connection.putrequest("GET", "/", skip_host=True)
+    http_connection.endheaders()
+    response = http_connection.getresponse()
+    actual_body = response.read()
+    expected_body = RESPONSE_BODY_400
+    assert response.status == 400
+    assert response.reason == "Bad Request"
+    assert response.version == 11
+    assert response.getheader("Content-Length") == str(len(expected_body))
+    assert actual_body == expected_body
+
+
+def test_negative_content_length_header(http_connection: HTTPConnection):
+    headers = {"Content-Length": "-1"}
+    http_connection.request("GET", "/", headers=headers)
+    response = http_connection.getresponse()
+    actual_body = response.read()
+    expected_body = RESPONSE_BODY_400
+    assert response.status == 400
+    assert response.reason == "Bad Request"
+    assert response.version == 11
+    assert response.getheader("Content-Length") == str(len(expected_body))
+    assert actual_body == expected_body
+
+
+def test_non_digit_content_length_header(http_connection: HTTPConnection):
+    headers = {"Content-Length": "123a"}
+    http_connection.request("GET", "/", headers=headers)
+    response = http_connection.getresponse()
+    actual_body = response.read()
+    expected_body = RESPONSE_BODY_400
     assert response.status == 400
     assert response.reason == "Bad Request"
     assert response.version == 11
