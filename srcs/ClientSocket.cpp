976acae79a6c3ff586dfd8adc286c55e6f5a7ec1
nfukada <nfukada@student.42tokyo.jp>
2021-12-20 19:11:18 +0900

    fix(ClientSocket): recvの戻り値型をintから関数定義のssize_tに修正

--- srcs/ClientSocket.cpp
+++ srcs/ClientSocket.cpp
@@ -22,7 +22,7 @@ ClientSocket::~ClientSocket()
 void ClientSocket::receiveRequest()
 {
     char buffer[BUF_SIZE];
-    int read_byte = recv(fd_, buffer, BUF_SIZE - 1, 0);
+    ssize_t read_byte = recv(fd_, buffer, BUF_SIZE - 1, 0);
     if (read_byte <= 0)
     {
         state_ = CLOSE;
