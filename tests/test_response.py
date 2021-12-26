a0fdcea5515e4e538bad023e686c6e7b03175a78
nfukada <nfukada@student.42tokyo.jp>
2021-12-26 15:30:55 +0900

    test: assert_responseを使ってテストをするように

--- tests/test_response.py
+++ tests/test_response.py
@@ -4,50 +4,29 @@ from tests.helper import assert_response
 
 
 def test_valid(http_connection: HTTPConnection):
+    """正常なGETリクエストで200を返すこと"""
     http_connection.request("GET", "/")
     response = http_connection.getresponse()
-    actual_body = response.read()
-    expected_body = RESPONSE_BODY_200
-    assert response.status == 200
-    assert response.reason == "OK"
-    assert response.version == 11
-    assert response.getheader("Content-Length") == str(len(expected_body))
-    assert actual_body == expected_body
-
-
-def test_header_server(http_connection: HTTPConnection):
-    http_connection.request("GET", "/")
-    response = http_connection.getresponse()
-    assert response.getheader("Server") == "webserv/1.0.0"
-
-
-def test_header_date(http_connection: HTTPConnection):
-    http_connection.request("GET", "/")
-    response = http_connection.getresponse()
-    date_value = response.getheader("Date")
-    datetime.strptime(date_value, "%a, %d %b %Y %H:%M:%S GMT")
+    assert_response(HTTPStatus.OK, response)
 
 
 def test_invalid_method(http_connection: HTTPConnection):
+    """サポートしていないmethodなら501を返すこと"""
     http_connection.request("INVALID", "/")
     response = http_connection.getresponse()
-    actual_body = response.read()
-    expected_body = RESPONSE_BODY_501
-    assert response.status == 501
-    assert response.reason == "Not Implemented"
-    assert response.version == 11
-    assert response.getheader("Content-Length") == str(len(expected_body))
-    assert actual_body == expected_body
+    assert_response(HTTPStatus.NOT_IMPLEMENTED, response)
 
 
 def test_invalid_http_version(http_connection: HTTPConnection):
+    """HTTP/1.0なら400を返すこと"""
     http_connection._http_vsn_str = "HTTP/1.0"
     http_connection.request("GET", "/")
     response = http_connection.getresponse()
-    actual_body = response.read()
-    expected_body = RESPONSE_BODY_400
-    assert response.status == 400
-    assert response.reason == "Bad Request"
-    assert response.version == 11
-    assert response.getheader("Content-Length") == str(len(expected_body))
-    assert actual_body == RESPONSE_BODY_400
+    assert_response(HTTPStatus.BAD_REQUEST, response)
+
+
+def test_404_no_such_file(http_connection: HTTPConnection):
+    """URIのファイルが存在しない時、404を返すこと"""
+    http_connection.request("GET", "/nosuchfile")
+    response = http_connection.getresponse()
+    assert_response(HTTPStatus.NOT_FOUND, response)
