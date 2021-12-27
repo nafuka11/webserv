17a635360e048b5c2ad86d63d85cc5d51bddc215
nfukada <nfukada@student.42tokyo.jp>
2021-12-27 18:05:06 +0900

    test(response): autoindexのHTMLを確認するテストを追加

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,5 +1,7 @@
+import re
 from http import HTTPStatus
 from http.client import HTTPConnection
+
 from tests.helper import assert_response
 
 
@@ -30,3 +32,22 @@ def test_404_no_such_file(http_connection: HTTPConnection):
     http_connection.request("GET", "/nosuchfile")
     response = http_connection.getresponse()
     assert_response(HTTPStatus.NOT_FOUND, response)
+
+
+def test_autoindex(http_connection: HTTPConnection):
+    """autoindex onのディレクトリで各ファイル情報のHTMLが返されること"""
+    http_connection.request("GET", "/autoindex/")
+    response = http_connection.getresponse()
+    actual_body = response.read().decode("utf-8")
+    expected_files = [
+        r'<a href="subdir/">subdir/</a>                                            \d{2}-\w+-\d{4} \d{2}:\d{2} +-',
+        r'<a href="50_name_len_directory_2345678901234567890123456789/">50_name_len_directory_2345678901234567890123456\.\.&gt;</a> \d{2}-\w+-\d{4} \d{2}:\d{2} +-',
+        r'<a href="49_name_len_directory_345678901234567890123456789/">49_name_len_directory_345678901234567890123456789/</a> \d{2}-\w+-\d{4} \d{2}:\d{2} +-',
+        r'<a href="50_name_len_file_2345678901234567890123456789\.html">50_name_len_file_2345678901234567890123456789\.html</a> \d{2}-\w+-\d{4} \d{2}:\d{2} +\d+',
+        r'<a href="51_name_len_file_12345678901234567890123456789\.html">51_name_len_file_12345678901234567890123456789\.\.\.&gt;</a> \d{2}-\w+-\d{4} \d{2}:\d{2} +\d+',
+        r'<a href="escape%26%3c%22char%22%3e\.html">escape&amp;&lt;&quot;char&quot;&gt;\.html</a>                               \d{2}-\w+-\d{4} \d{2}:\d{2} +\d+',
+        r'<a href="%3chtml%3e\.html">&lt;html&gt;\.html</a>                                        \d{2}-\w+-\d{4} \d{2}:\d{2} +\d+',
+    ]
+    for pattern in expected_files:
+        assert re.search(pattern, actual_body) is not None
+    assert response.status == 200
