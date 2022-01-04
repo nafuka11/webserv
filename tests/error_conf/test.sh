879c5c8f3f6517df6c3fa7995c21dae9e76f1ed5
ysaito <yksaito02@gmail.com>
2022-01-04 18:05:02 +0900

    fix(tests/error_conf/): test.sh内部の処理を関数に切り出し。スクリプト実行時、実行したいファイルのキーワードを渡せるように変更

--- tests/error_conf/test.sh
+++ tests/error_conf/test.sh
@@ -5,20 +5,37 @@ CONF_DIR="./conf/error"
 COLOR_TEST="\033[36m"
 COLOR_RESET="\033[0m"
 
+run_test () {
+  file="$1"
+  echo
+  printf "${COLOR_TEST}test: ${file}${COLOR_RESET}\n"
+  "${EXE_PATH}" "${file}" 2>&1 | tail -1
+}
+
+run_tests () {
+  for file in $(find "${CONF_DIR}" -name "$1" | sort); do
+    run_test "${file}"
+  done
+}
+
+show_usage () {
+  grep 'if \[' tests/error_conf/test.sh | awk -F'\"|## ' '{printf"    \033[36m%-10s\033[0m %s\n", $4, $6}'
+}
+
+main () {
 if [ $# -eq 0 -o "$1" = "all" ] ; then ## Test all config error file
-  for file in $(find "${CONF_DIR}" -name "*.conf" | sort); do
-    echo
-    printf "${COLOR_TEST}test: ${file}${COLOR_RESET}\n"
-    "${EXE_PATH}" "${file}" 2>&1 | tail -1
-  done
+  run_tests "*.conf"
+elif [ "$1" = "context" ] ; then ## Test error for context(server, location)
+  run_tests "context_*.conf"
+elif [ "$1" = "directive" ] ; then ## Test error for directive
+  run_tests "directive_*.conf"
 elif [ "$1" = "semicolon" ] ; then ## Test error for semicolon(;)
-  for file in $(find "${CONF_DIR}" -name "*_semicolon.conf" | sort); do
-    echo
-    printf "${COLOR_TEST}test: ${file}${COLOR_RESET}\n"
-    "${EXE_PATH}" "${file}" 2>&1 | tail -1
-  done
+  run_tests "semicolon_*.conf"
 elif [ "$1" = "help" ] ; then ## Show this message
-  grep if tests/error_conf/test.sh | awk -F'\"|## ' '{printf"    \033[36m%-10s\033[0m %s\n", $4, $6}'
+  show_usage
 else
-  grep if tests/error_conf/test.sh | awk -F'\"|## ' '{printf"    \033[36m%-10s\033[0m %s\n", $4, $6}'
+  show_usage
 fi
+}
+
+main $@
