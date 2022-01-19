72ef0a9822039be1af76fe68cd195775f9f94121
nfukada <nfukada@student.42tokyo.jp>
2022-01-19 13:51:39 +0900

    feat(HTTPResponse): setPropertiesでStatusCodeに応じてHTMLを生成しないように
    HTML生成はhandleErrorで行います

--- srcs/HTTPResponse.cpp
+++ srcs/HTTPResponse.cpp
@@ -77,10 +77,6 @@ std::map<HTTPStatusCode, std::string> HTTPResponse::setReasonPhrase()
 
 void HTTPResponse::setProperties(const LocationConfig *location)
 {
-    if (status_code_ != CODE_200)
-    {
-        message_body_ = generateHTMLfromStatusCode(status_code_);
-    }
     std::stringstream ss;
     ss << message_body_.size();
     std::string content_length = ss.str();
