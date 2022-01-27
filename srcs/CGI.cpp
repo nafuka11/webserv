a9c14201e1403f544e36c548a134398c8299962b
ysaito <shiva.lts239@gmail.com>
2022-01-27 22:06:44 +0900

    feat(CGI.cpp): execveに渡す環境変数を作成する処理の雛形を実装

--- srcs/CGI.cpp
+++ srcs/CGI.cpp
@@ -1,4 +1,5 @@
 #include "CGI.hpp"
+#include <vector>
 #include <unistd.h>
 #include "SystemError.hpp"
 
@@ -38,16 +39,44 @@ void CGI::setArgv(const std::string &path)
     argv_ = new char*[3];
     std::string command = "php";
 
-    argv_[0] = new char[command.size() + 1];
-    std::char_traits<char>::copy(argv_[0], command.c_str(), command.size() + 1);
-    argv_[1] = new char[path_.size() + 1];
+    argv_[0] = new char[std::strlen("php") + 1];
+    std::char_traits<char>::copy(argv_[0], "php", std::strlen("php") + 1);
+    argv_[1] = new char[path.size() + 1];
     std::char_traits<char>::copy(argv_[1], path.c_str(), path.size() + 1);
     argv_[2] = NULL;
 }
 
 void CGI::setEnvp()
 {
-    envp_ = NULL;
+    std::vector<std::string> envs;
+    envs.push_back(std::string("AUTH_TYPE=") + std::string("test"));
+    envs.push_back(std::string("CONTENT_LENGTH=") + std::string("test"));
+    envs.push_back(std::string("CONTENT_TYPE=") + std::string("test"));
+    envs.push_back(std::string("GATEWAY_INTERFACE=") + std::string("test"));
+    envs.push_back(std::string("PATH_INFO=") + std::string("test"));
+    envs.push_back(std::string("PATH_TRANSLATED=") + std::string("test"));
+    envs.push_back(std::string("QUERY_STRING=") + std::string("test"));
+    envs.push_back(std::string("REMOTE_ADDR=") + std::string("test"));
+    envs.push_back(std::string("REMOTE_HOST=") + std::string("test"));
+    envs.push_back(std::string("REMOTE_IDENT=") + std::string("test"));
+    envs.push_back(std::string("REMOTE_USER=") + std::string("test"));
+    envs.push_back(std::string("REQUEST_METHOD=") + std::string("test"));
+    envs.push_back(std::string("SCRIP_NAME=") + std::string("test"));
+    envs.push_back(std::string("SERVER_NAME=") + std::string("test"));
+    envs.push_back(std::string("SERVER_PORT=") + std::string("test"));
+    envs.push_back(std::string("SERVER_PROTOCOL=") + std::string("test"));
+    envs.push_back(std::string("SERVER_SOFTWARE=") + std::string("test"));
+
+    envp_ = new char*[18];
+    size_t index = 0;
+    for (std::vector<std::string>::const_iterator env = envs.begin();
+         env != envs.end();
+         env++, index++)
+    {
+        envp_[index] = new char[env->size() + 1];
+        std::char_traits<char>::copy(envp_[index], env->c_str(), env->size() + 1);
+    }
+    envp_[index] = NULL;
 }
 
 void CGI::Execute()
