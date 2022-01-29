4bc510499566c51bbd7cfd82f32c4ae35e0ea9ea
nfukada <nfukada@student.42tokyo.jp>
2022-01-29 17:53:58 +0900

    test(conf_test): actual/にエラーメッセージを出力してexpected/とdiffを取るように

--- tests/conf_test/test.sh
+++ tests/conf_test/test.sh
@@ -10,23 +10,40 @@ ACTUAL_DIR="./actual"
 
 COLOR_TEST="\033[36m"
 COLOR_FILE_COUNT="\033[33m"
+COLOR_OK="\033[32m"
+COLOR_KO="\033[31m"
 COLOR_RESET="\033[0m"
 
+show_usage () {
+  echo "Usage: $0 [ Search Keyword ]"
+}
+
+assert_output () {
+  local actual_file="$1"
+  local expected_file="$2"
+  local conf_file="$3"
+  local diff_result=$(diff "${actual_file}" "${expected_file}")
+  if [ $? -eq 0 ]; then
+    printf "[${COLOR_OK}OK${COLOR_RESET}] ${conf_file}\n"
+  else
+    printf "[${COLOR_KO}KO${COLOR_RESET}] ${conf_file}\n"
+    echo "${diff_result}"
+  fi
+}
+
 run_test () {
-  file="$1"
-  echo
-  printf "${COLOR_TEST}test: ${file}${COLOR_RESET}\n"
-  "${EXE_PATH}" "${file}" 2>&1 | tail -1
+  local conf_file="$1"
+  local txt_file=$(basename "${conf_file}" | sed 's/\(.*\)\.[^\.]*$/\1.txt/')
+  local expected_file="${EXPECTED_DIR}/${txt_file}"
+  local actual_file="${ACTUAL_DIR}/${txt_file}"
+  "${EXE_PATH}" "${conf_file}" 2>&1 | tail -1 > "${expected_file}"
+  assert_output "${expected_file}" "${actual_file}" "${conf_file}"
 }
 
 run_tests () {
   for file in "${array[@]}"; do
     run_test "${file}"
   done
-}
-
-show_usage () {
-  echo "Usage: $0 [ Search Keyword ]"
 }
 
 main () {
@@ -36,7 +53,6 @@ if [ $# -eq 0 ] ; then
 else
   array=( $(find "${CONF_DIR}" -name "*$1*.conf" | sort) )
   if [ ${#array[*]} -ne 0 ] ; then
-    echo
     if [ ${#array[*]} -eq 1 ] ; then
       printf "${COLOR_FILE_COUNT}KEYWORD: \"$1\"\n▶︎ ${#array[*]} file find !${COLOR_RESET}\n"
     else
