cf3ed234109fbb6554d5ad5814d76a64cc284c74
nfukada <nfukada@student.42tokyo.jp>
2022-01-30 16:23:29 +0900

    test(http_test): redirectのテストを追加

---
+++ tests/http_test/test_redirect.py
@@ -0,0 +1,31 @@
+from http import HTTPStatus
+from http.client import HTTPConnection
+
+import pytest
+
+from http_test.helper import assert_response
+
+
+class TestRedirect:
+    @pytest.mark.parametrize(
+        "status",
+        [
+            HTTPStatus.MOVED_PERMANENTLY,
+            HTTPStatus.FOUND,
+            HTTPStatus.SEE_OTHER,
+            HTTPStatus.NOT_MODIFIED,
+            HTTPStatus.TEMPORARY_REDIRECT,
+            HTTPStatus.PERMANENT_REDIRECT,
+        ],
+    )
+    def test_reason_phrase(self, http_connection: HTTPConnection, status: HTTPStatus):
+        http_connection.request("GET", f"/redirect_{status.value}/")
+        response = http_connection.getresponse()
+        assert_response(status, response, empty_body=True)
+
+    @pytest.mark.parametrize("status", [104, 999])
+    def test_no_reason_phrase(self, http_connection: HTTPConnection, status: int):
+        http_connection.request("GET", f"/redirect_{status}/")
+        response = http_connection.getresponse()
+        assert response.status == status
+        assert response.reason == ""
